4de053dc5f12217a2d58a57fe2a709d85d698476
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-04-24 10:57:16 -0300

    array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite qual é quantidade de pessoas que deseja fazer a média da altura: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.Write("Digite a altura: ");
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i];
+            }
+            double avg = sum / n;
+            
+            Console.WriteLine($"AVERAGE HEIGHT = {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
